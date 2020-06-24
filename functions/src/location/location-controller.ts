3582e166e4d2ab2d5303044a1c26f397f0065c54
Iman Afaneh <iman.afaneh.3@gmail.com>
2020-06-24 16:50:47 +0300

    adding read

--- functions/src/location/location-controller.ts
+++ functions/src/location/location-controller.ts
@@ -1,23 +1,42 @@
 import * as functions from 'firebase-functions';
-import {user } from './location';
+//import {user } from './location';
 import { db } from '../index'
 //const path = require('path');
 //const os = require('os');
 //const fs = require('fs');
 //const Busboy = require('busboy');
-const userCollection = 'users';
-
-exports.addlocation = functions.https.onCall(async (data, context)=>{
-    if (!context.auth)
-        return {message: 'Authentication required', code: 401 };
-    const d = data.text;
-    const u : user ={
-        location:{
-            lang: d.location.lang,
-            lat: d.location.lat
-        }
+//const userCollection = 'users';
+//const locationRef = db.collection('users').doc('location');
+/*let getDoc = locationRef.get()
+  .then(doc => {
+    if (!doc.exists) {
+      console.log('No such document!');
+    } else {
+      console.log('Document data:', doc.data());
     }
-    const newDoc = await db.collection(userCollection).add(u);
-    return {newDoc}
-    
+  })
+  .catch(err => {
+    console.log('Error getting document', err);
+  });*/
+exports.readLocation = functions.https.onCall((data, context)=>{
+    const locationRef = db.collection('users');
+    return locationRef.get()
+  .then(snapshot => {
+    snapshot.forEach(doc => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch(err => {
+    console.log('Error getting documents', err);
+  });   
 });
+/*locationRef.listCollections().then(collections => {
+    collections.forEach(collection => {
+      console.log('Found subCollection with id:', collection.id);
+    });
+  });
+  return {console.log(data.text)};*/
+  /*exports.findDistance = functions.firestore
+    .document('users/location').onWrite((change, context) => {
+      // ... Your code here
+    });*/
